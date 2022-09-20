270418e4c78ea43c36e078427caa394df14efac4
Jeff Marks <jeff.t.marks@gmail.com>
2022-09-20 10:51:19 -0400

    done with bonus

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,22 +1,46 @@
 require 'pry'
 
 class Stack
-	attr_reader :stack
 
-	def initialize
+	def initialize(limit = nil)
 		@stack = []
+		@limit = limit
 	end
 
 	def push(value)
-		self.stack.push(value)
+		if self.full?
+			raise "Stack Overflow"
+		else
+			@stack.push(value)
+		end
 	end
 
 	def pop
-		self.stack.pop
+		@stack.pop
 	end
 
 	def peek
-		self.stack.last
+		@stack.last
+	end
+
+	def size
+		@stack.size
+	end
+
+	def empty?
+		@stack.empty?
+	end
+
+	def full?
+		@stack.size == @limit
+	end
+
+	def search(value)
+		@stack.each_with_index do | item, index |
+			return self.size - index - 1 if item == value
+		end
+
+		-1
 	end
 
 end
