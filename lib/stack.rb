fe53de54f8fe16378db214c1bb57069b058fc422
Jeff Marks <jeff.t.marks@gmail.com>
2022-09-20 10:33:56 -0400

    done with assignment

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,24 @@
-# your code here
+require 'pry'
+
+class Stack
+	attr_reader :stack
+
+	def initialize
+		@stack = []
+	end
+
+	def push(value)
+		self.stack.push(value)
+	end
+
+	def pop
+		self.stack.pop
+	end
+
+	def peek
+		self.stack.last
+	end
+
+end
+
+# binding.pry
